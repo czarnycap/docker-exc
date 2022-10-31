a6c09356c59f3804573d526c1b308f1457b03d07
Ian Oginski <Ian.Oginski@protonmail.com>
2022-10-31 12:49:49 +0000

    changed .env

---
+++ all-dockers-altogheter/env_recreation_scripts/add_storage_base_on_compose.sh
@@ -0,0 +1,6 @@
+#requires yq tool, could be installed with snap
+
+COMPOSE_PATH=../
+yq -r '.volumes' $COMPOSE_PATH/docker-compose.yml |grep -v external|sed 's/\://g' > ./storage_list
+#run rather in console than as a script
+for i in $(cat ./storage_list); do  docker volume create $i; done
